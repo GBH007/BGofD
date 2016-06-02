0952a4625ae75094cc7263aa5b3dfd5823c0751f
Григорий Никониров <mrgbh007@gmail.com>
2016-06-02 12:01:25 +0700

    первичное изменение классов под тк

--- src/display.py
+++ src/display.py
@@ -21,7 +21,7 @@ class Textures:
 		for i in glob.glob(self.__PATH_TO_MAP+'/texture/*.gif'):
 			self.__tx[os.path.split(i)[1].split('.')[0]]=tkinter.PhotoImage(file=i)
 	def get(self,name):
-		return self.__tx.get(name,tkinter.PhotoImage())
+		return self.__tx.get(name,None)
 		
 		
 
@@ -36,119 +36,135 @@ TYPE_INFO={
 }
 
 
-def str_parser17(s):
-	cache=s[:]
-	s=[]
-	for i in cache.split('\n'):
-		if i:
-			for j in range(len(i)//17+1):
-				s.append(i[:17])
-				i=i[17:]
-	return s
+#~ def str_parser17(s):
+	#~ cache=s[:]
+	#~ s=[]
+	#~ for i in cache.split('\n'):
+		#~ if i:
+			#~ for j in range(len(i)//17+1):
+				#~ s.append(i[:17])
+				#~ i=i[17:]
+	#~ return s
 
 
 class ChatDisplay:
 	# __PATH_TO_MAP='/home/gbh007/Dropbox/PG/black_engine_python/'
 
-	def __init__(self, PATH_TO_MAP):
+	def __init__(self, PATH_TO_MAP,canvas):
 		self.__PATH_TO_MAP=PATH_TO_MAP
-		self._display=[]
-		cache=open(self.__PATH_TO_MAP+'map/chat.log').readlines()
-		for i in cache[-20:]:
-			for j in str_parser17(i):
-				self._display.append(j.strip('\n'))
+		self.__canvas=canvas
+		#~ self._display=[]
+		#~ cache=open(self.__PATH_TO_MAP+'map/chat.log').readlines()
+		#~ for i in cache[-20:]:
+			#~ for j in str_parser17(i):
+				#~ self._display.append(j.strip('\n'))
 
 	def addMessage(self, message):
-		for i in str_parser17(message):
-			self._display.append(i.strip('\n'))
+		pass
+		#~ for i in str_parser17(message):
+			#~ self._display.append(i.strip('\n'))
 
 	def getLast20(self):
-		s=self._display[-20:]
-		if len(s)<20:
-			for i in range(20-len(s)):
-				s.insert(0, '')
-		return s
+		pass
+		#~ s=self._display[-20:]
+		#~ if len(s)<20:
+			#~ for i in range(20-len(s)):
+				#~ s.insert(0, '')
+		#~ return s
 
 	def destructor(self):
-		f=open(self.__PATH_TO_MAP+'map/chat.log', 'w')
-		for i in self._display:
-			print(i, file=f)
+		pass
+		#~ f=open(self.__PATH_TO_MAP+'map/chat.log', 'w')
+		#~ for i in self._display:
+			#~ print(i, file=f)
 
 
-class _MainDisplay:
-	def __init__(self):
-		self._display=[]
+#~ class _MainDisplay:
+	#~ def __init__(self):
+		#~ self._display=[]
 
-	def refreshMap(self, map_data):
-		self._display=[]
-		self._display=[''.join([TYPE_INFO.get(j, ' ') for j in i]) for i in map_data]
-		self._display.reverse()
+	#~ def refreshMap(self, map_data):
+		#~ self._display=[]
+		#~ self._display=[''.join([TYPE_INFO.get(j, ' ') for j in i]) for i in map_data]
+		#~ self._display.reverse()
 
-	def getMap(self):
-		return self._display
-
-
-class MapDisplay(_MainDisplay): pass
+	#~ def getMap(self):
+		#~ return self._display
 
 
-class EntityDisplay(_MainDisplay): pass
+#~ class MapDisplay(_MainDisplay): pass
+
+
+#~ class EntityDisplay(_MainDisplay): pass
 
 
 class MainDisplay:
-	def __init__(self):
-		self._map_display=MapDisplay()
-		self._entity_display=EntityDisplay()
+	def __init__(self,canvas):
+		self.__canvas=canvas
+		#~ self.__canvas.pack()
+		#~ self._map_display=MapDisplay()
+		#~ self._entity_display=EntityDisplay()
 
 	def refreshEntityMap(self, map_data):
-		self._entity_display.refreshMap(map_data)
+		pass
+		#~ self._entity_display.refreshMap(map_data)
 
 	def refreshMap(self, map_data):
-		self._map_display.refreshMap(map_data)
+		pass
+		#~ self._map_display.refreshMap(map_data)
 
 	def getMap(self):
-		cache=self._map_display.getMap()
-		cache1=self._entity_display.getMap()
-		s=[]
-		for i in range(len(cache1)):
-			s1=''
-			for j in range(len(cache1[i])):
-				s1+=cache1[i][j] if cache[i][j]==' ' else cache[i][j]
-			s.append(s1)
-		return s
+		pass
+		#~ cache=self._map_display.getMap()
+		#~ cache1=self._entity_display.getMap()
+		#~ s=[]
+		#~ for i in range(len(cache1)):
+			#~ s1=''
+			#~ for j in range(len(cache1[i])):
+				#~ s1+=cache1[i][j] if cache[i][j]==' ' else cache[i][j]
+			#~ s.append(s1)
+		#~ return s
 
 
 class StatDisplay:
-	def __init__(self):
-		self._display=[]
+	def __init__(self,canvas):
+		self.__canvas=canvas
+		#~ self.__canvas.pack()
+		#~ self._display=[]
 
 	def refresh(self, entity_stats):
-		self._display=[]
-		for i in entity_stats:
-			for j in str_parser17(i):
-				self._display.append(j)
+		pass
+		#~ self._display=[]
+		#~ for i in entity_stats:
+			#~ for j in str_parser17(i):
+				#~ self._display.append(j)
 
 	def getStats(self):
-		s=self._display[:10]
-		if len(s)<10:
-			for i in range(10-len(s)):
-				s.append('')
-		return s
+		pass
+		#~ s=self._display[:10]
+		#~ if len(s)<10:
+			#~ for i in range(10-len(s)):
+				#~ s.append('')
+		#~ return s
 
 
 class Display:
-	line_templates={
-		'1': '#'*50,
-		'2': '#{0:17}#{1:30}#',
-		'3': '#'*19+'{0:30}#'
-	}
+	#~ line_templates={
+		#~ '1': '#'*50,
+		#~ '2': '#{0:17}#{1:30}#',
+		#~ '3': '#'*19+'{0:30}#'
+	#~ }
 
 	def __init__(self, PATH_TO_MAP):
+		self.__root=tkinter.Tk()
+		self.__canvas=tkinter.Canvas(self.__root,width=1100,height=900)
+		self.__canvas.pack()
 		self.__PATH_TO_MAP=PATH_TO_MAP
-		self._display=[]
-		self._chat_display=ChatDisplay(self.__PATH_TO_MAP)
+		#~ self._display=[]
+		self._stat_dispaly=StatDisplay(self.__canvas)
+		self._main_display=MainDisplay(self.__canvas)
+		self._chat_display=ChatDisplay(self.__PATH_TO_MAP,self.__canvas)
 		# self._map_display=MapDisplay()
-		self._main_display=MainDisplay()
-		self._stat_dispaly=StatDisplay()
 
 	def refreshMap(self, map_data):
 		self._main_display.refreshMap(map_data)
@@ -163,24 +179,27 @@ class Display:
 		self._chat_display.addMessage(message)
 
 	def refresh(self):
-		self._display=[]
-		map_cache=self._main_display.getMap()
-		chat_cache=self._chat_display.getLast20()
-		stat_cache=self._stat_dispaly.getStats()
-		for i in range(32):
-			if i==0:
-				self._display.append(self.line_templates['1'])
-			elif i<10:
-				self._display.append(self.line_templates['2'].format(stat_cache[i-1], map_cache[i-1]))
-			elif i==10:
-				self._display.append(self.line_templates['3'].format(map_cache[i-1]))
-			elif i<31:
-				self._display.append(self.line_templates['2'].format(chat_cache[i-11], map_cache[i-1]))
-			else:
-				self._display.append(self.line_templates['1'])
+		self.__root.mainloop()
+		#~ pass
+		#~ self._display=[]
+		#~ map_cache=self._main_display.getMap()
+		#~ chat_cache=self._chat_display.getLast20()
+		#~ stat_cache=self._stat_dispaly.getStats()
+		#~ for i in range(32):
+			#~ if i==0:
+				#~ self._display.append(self.line_templates['1'])
+			#~ elif i<10:
+				#~ self._display.append(self.line_templates['2'].format(stat_cache[i-1], map_cache[i-1]))
+			#~ elif i==10:
+				#~ self._display.append(self.line_templates['3'].format(map_cache[i-1]))
+			#~ elif i<31:
+				#~ self._display.append(self.line_templates['2'].format(chat_cache[i-11], map_cache[i-1]))
+			#~ else:
+				#~ self._display.append(self.line_templates['1'])
 
 	def __str__(self):
-		return '\n'.join(self._display)
+		pass
+		#~ return '\n'.join(self._display)
 
 	def destructor(self):
 		self._chat_display.destructor()
@@ -190,12 +209,13 @@ class Display:
 
 
 def main():
-	c=scord.Cord()
-	d=Display()
-	a=schart.Map()
-	d.refreshMap(a.getMap(c))
+	#~ c=scord.Cord()
+	d=Display('.')
 	d.refresh()
-	print(d)
+	#~ a=schart.Map()
+	#~ d.refreshMap(a.getMap(c))
+	#~ d.refresh()
+	#~ print(d)
 
 
 # print(a._game_objects)
